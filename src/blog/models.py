83727cb6867beaccc98b3faf9466ac77d7cabea9
CFE <hello@teamcfe.com>
2016-10-24 16:06:23 -0700

    14 - Error Messages & Help Text

--- src/blog/models.py
+++ src/blog/models.py
@@ -19,7 +19,15 @@ PUBLISH_CHOICES = [
 class PostModel(models.Model):
     id              = models.BigAutoField(primary_key=True)
     active          = models.BooleanField(default=True) #empty in the database
-    title           = models.CharField(max_length=240, verbose_name='Post title', unique=True)
+    title           = models.CharField(
+                            max_length=240, 
+                            verbose_name='Post title', 
+                            unique=True,
+                            error_messages={
+                                "unique": "This title is not unique, please try again.",
+                                "blank": "This field is not full, please try again."
+                            },
+                            help_text='Must be a unique title.')
     slug            = models.SlugField(null=True, blank=True)
     content         = models.TextField(null=True, blank=True)
     publish         = models.CharField(max_length=120, choices=PUBLISH_CHOICES, default='draft')
