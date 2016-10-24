7687c57958fbfd6c7411611cb75377a7fb030e59
CFE <hello@teamcfe.com>
2016-10-24 15:36:48 -0700

    12 - Overriding the Save Method

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.encoding import smart_text
 from django.utils import timezone 
+from django.utils.text import slugify
 # Create your models here.
 
 
@@ -16,11 +17,17 @@ class PostModel(models.Model):
     id              = models.BigAutoField(primary_key=True)
     active          = models.BooleanField(default=True) #empty in the database
     title           = models.CharField(max_length=240, verbose_name='Post title', unique=True)
+    slug            = models.SlugField(null=True, blank=True)
     content         = models.TextField(null=True, blank=True)
     publish         = models.CharField(max_length=120, choices=PUBLISH_CHOICES, default='draft')
     view_count      = models.IntegerField(default=0)
     publish_date    = models.DateField(auto_now=False, auto_now_add=False, default=timezone.now)
     author_email    = models.EmailField(max_length=240, validators=[validate_justin], null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug and self.title:
+            self.slug = slugify(self.title)
+        super(PostModel, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name = 'Post'
