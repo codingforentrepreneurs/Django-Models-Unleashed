b2db5a87611c5aa6eca6fd7b927ffb4bae113a01
CFE <hello@teamcfe.com>
2016-10-24 14:40:35 -0700

    8 - Django Field Choices

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 # Create your models here.
 
+PUBLISH_CHOICES = [
+        ('draft', 'Draft'),
+        ('publish', 'Publish'),
+        ('private', 'Private'),
+    ]
+
 class PostModel(models.Model):
     id          = models.BigAutoField(primary_key=True)
     active      = models.BooleanField(default=True) #empty in the database
     title       = models.CharField(max_length=240, verbose_name='Post title')
     content     = models.TextField(null=True, blank=True)
+    publish     = models.CharField(max_length=120, choices=PUBLISH_CHOICES, default='draft')
     #id = models.IntegerField(primary_key=True) #auto increments 1, 2, 3, 4, 
 
     class Meta:
@@ -17,5 +24,9 @@ class PostModel(models.Model):
 '''
 python manage.py makemigrations #every time you change models.py
 python manage.py migate
+
+
+ModelForm
+forms.CharField(max_length=120, choices=PUBLISH_CHOICES, default='draft')
 
 '''
