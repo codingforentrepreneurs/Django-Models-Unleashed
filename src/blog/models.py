d27e794ea8f8d808930d6b2a13978bfe64da2cb0
CFE <hello@teamcfe.com>
2016-10-24 14:10:39 -0700

    4 - App, First Model, & Makemigrations

---
+++ src/blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.db.models import Model
+# Create your models here.
+
+class PostModel(Model):
+    pass
+
+
+
+```
+python manage.py makemigrations
+python manage.py migate
+
+```
